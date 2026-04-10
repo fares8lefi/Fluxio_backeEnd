c47d8f73b2978bf1ad7974c47c641bd8824d35e2
fares8lefi <fareslefi4@gmail.com>
2026-04-10 21:31:31 +0200

    refactor: simplify router logic

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,12 +6,21 @@ const {requireAuthUser}= require("../middlewares/authMiddelwares");
 
 const {isAdmin}= require("../middlewares/isAdminMiddelware");
 /* GET users listing. */
+//post
 router.post('/createUser',userController.createUser)
 router.post('/loginUser',userController.loginUser)
-router.get('/getConnectedUser',requireAuthUser,userController.getConnectedUser)
 router.post('/logOutUser', requireAuthUser, userController.logOutUser);
 router.post('/changePassword',requireAuthUser,userController.changePassword)
+router.post('/resendCode',userController.resendCode)
+
+//put
 router.put('/updatePersonnelData',requireAuthUser,userController.updatePersonnelData)
 router.put('/updateUserStatus',requireAuthUser,userController.updateUserStatus)
+router.put('/foorgetPasswordVerifyCode',userController.foorgetPasswordVerifyCode)
+router.put('/verifyAccounts',userController.verifyAccounts)
+
+
+//get
 router.get('/getAllUsers',requireAuthUser,isAdmin,userController.getAllUsers)
+router.get('/getConnectedUser',requireAuthUser,isAdmin,userController.getConnectedUser)
 module.exports = router;

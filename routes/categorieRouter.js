2ae3ef2715aea8005ae9de05a57fb3c36d75da78
fares8lefi <fareslefi4@gmail.com>
2026-04-18 15:49:41 +0200

    feat: secure category API with auth middleware

--- routes/categorieRouter.js
+++ routes/categorieRouter.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const categorieController =require('../Controllers/categorieController')
+const {requireAuthUser}= require('../middlewares/authMiddelwares')
 
-
-router.post('/createCategorie', categorieController.createcategory);
-router.get('/getAllCategories', categorieController.getAllCategories);
-router.put('/updateCategorie', categorieController.updateCategorie);
-router.delete('/deleteCategorie/:id', categorieController.deleteCategorie);
+router.post('/createCategorie',requireAuthUser, categorieController.createcategory);
+router.get('/getAllCategories',requireAuthUser, categorieController.getAllCategories);
+router.put('/updateCategorie/:id',requireAuthUser, categorieController.updateCategorie);
+router.delete('/deleteCategorie/:id',requireAuthUser, categorieController.deleteCategorie);
 
 module.exports = router;

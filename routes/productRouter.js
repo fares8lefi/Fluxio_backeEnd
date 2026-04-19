1aee9b054796b8bacd65b20c50de263f3f52b974
fares8lefi <fareslefi4@gmail.com>
2026-04-19 21:53:35 +0200

    feat: add get product by id route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,6 +8,6 @@ router.post('/addProduct', requireAuthUser, productController.addProduct);
 router.put('/updateProduct/:id',requireAuthUser, productController.updateProduct);
 router.delete('/deleteProduct/:id',requireAuthUser, productController.deleteProduct);
 router.get('/getAllProduct',requireAuthUser, productController.getAllProduct);
-
+router.get('/getProductById/:id',requireAuthUser, productController.getProductById);
 
 module.exports = router;

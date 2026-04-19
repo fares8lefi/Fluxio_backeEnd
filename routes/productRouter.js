50f32fb456f037f43333d4cddf419b397da39eea
fares8lefi <fareslefi4@gmail.com>
2026-04-19 22:18:18 +0200

    feat: implement routing for filtering function

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,10 +4,15 @@ const productController =require('../Controllers/productController')
 const { requireAuthUser } = require('../middlewares/authMiddelwares');
 
 
+//post
 router.post('/addProduct', requireAuthUser, productController.addProduct);
+//put
 router.put('/updateProduct/:id',requireAuthUser, productController.updateProduct);
+//delete
 router.delete('/deleteProduct/:id',requireAuthUser, productController.deleteProduct);
+
+//get
 router.get('/getAllProduct',requireAuthUser, productController.getAllProduct);
 router.get('/getProductById/:id',requireAuthUser, productController.getProductById);
-
+router.get('/getProductByFiltres',requireAuthUser, productController.getProductByFiltres);
 module.exports = router;

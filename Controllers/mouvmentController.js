943559b08c96fc8b66c32e656e3308fd026b163a
fares8lefi <fareslefi4@gmail.com>
2026-04-22 17:49:18 +0200

    feat: implement function to create movement

--- Controllers/mouvmentController.js
+++ Controllers/mouvmentController.js
@@ -1,9 +1,10 @@
 const mouvmentModel = require('../models/mouvmentModel');
 
-
+const productModel = require('../models/productModel');
+const { validateMouvmentRegistration } = require('../validations/mouvmentValidations');
 const increaseStock = async (productId, quantity) => {
     try {
-        const product = await mouvmentModel.findById(productId);
+        const product = await productModel.findById(productId);
         if (!product) {
             throw new Error('Product not found');
         }
@@ -26,3 +27,30 @@ const decreaseStock = async (productId, quantity) => {
         throw error;
     }
 };  
+
+
+module.exports.createMouvment = async (req , res)=>{
+    try{
+        
+        const {type,unit,product,supplier,reference,note ,status}= req.body;
+        const validationResult = validateMouvmentRegistration(req.body);
+        if (!validationResult.isValid) {
+            return res.status(400).json({success: false, message: validationResult.errors});
+        }
+        
+        await mouvmentModel.create({
+            type,
+            unit,
+            product,
+            supplier,
+            reference,
+            note,
+            status
+        });
+        
+    
+        res.status(200).json({message: "Mouvment created successfully" , success: true});
+    }catch(error){
+        res.status(500).json({message: error.message , success: false});
+    }
+}

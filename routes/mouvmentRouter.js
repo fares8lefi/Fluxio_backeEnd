6e52ab40e2c84901acd4a279c6d67b4be442d8d4
fares8lefi <fareslefi4@gmail.com>
2026-04-22 18:32:03 +0200

    feat: implement get all movement routing

--- routes/mouvmentRouter.js
+++ routes/mouvmentRouter.js
@@ -4,5 +4,6 @@ const mouvmentController = require('../Controllers/mouvmentController');
 const { requireAuthUser } = require('../middlewares/authMiddelwares');
 
 router.post('/createMouvment',requireAuthUser, mouvmentController.createMouvment);
+router.get('/getAllMouvment',requireAuthUser, mouvmentController.getAllMouvment);
  
 module.exports = router;

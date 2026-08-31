3c21a8d4b924f82e49a5edef323bae7961e6688c
fares8lefi <fareslefi4@gmail.com>
2026-08-31 12:57:26 +0200

    feat: expose routes for filters and client/supplier history

--- src/routes/mouvmentRouter.js
+++ src/routes/mouvmentRouter.js
@@ -5,6 +5,9 @@ const { requireAuthUser } = require('../middlewares/authMiddelwares');
 
 router.post('/createMouvment',requireAuthUser, mouvmentController.createMouvment);
 router.get('/getAllMouvment',requireAuthUser, mouvmentController.getAllMouvment);
+router.get('/getByClient/:clientId', requireAuthUser, mouvmentController.getMouvmentsByClient);
+router.get('/getBySupplier/:supplierId', requireAuthUser, mouvmentController.getMouvmentsBySupplier);
 router.put('/cancel/:id', requireAuthUser, mouvmentController.cancelMouvment);
+router.get('/:id', requireAuthUser, mouvmentController.getMouvmentById);
  
 module.exports = router;

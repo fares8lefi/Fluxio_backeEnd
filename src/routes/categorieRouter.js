ce48829820c029a9364b6cfe6f956632c75f4826
fares8lefi <fareslefi4@gmail.com>
2026-09-01 13:45:28 +0200

    feat: expose category details and search routes

--- src/routes/categorieRouter.js
+++ src/routes/categorieRouter.js
@@ -5,7 +5,10 @@ const {requireAuthUser}= require('../middlewares/authMiddelwares')
 
 router.post('/createCategorie',requireAuthUser, categorieController.createcategory);
 router.get('/getAllCategories',requireAuthUser, categorieController.getAllCategories);
+router.get('/searchByName',requireAuthUser, categorieController.searchCategoriesByName);
 router.put('/updateCategorie/:id',requireAuthUser, categorieController.updateCategorie);
 router.delete('/deleteCategorie/:id',requireAuthUser, categorieController.deleteCategorie);
+// Route paramétrique en dernier
+router.get('/:id',requireAuthUser, categorieController.getCategorieById);
 
 module.exports = router;

58d867c04fef1af8a04887cbce03eabfd9d72b00
fares8lefi <fareslefi4@gmail.com>
2026-09-01 13:48:27 +0200

    feat: create endpoint  get client by id function

--- src/routes/clientRouter.js
+++ src/routes/clientRouter.js
@@ -4,11 +4,12 @@ const clientController = require('../controllers/clientController');
 const { requireAuthUser } = require('../middlewares/authMiddelwares');
 
 // Toutes les routes clients requièrent une authentification
-router.post('/createClient',                  requireAuthUser, clientController.createClient);
-router.put('/updateClient/:id',               requireAuthUser, clientController.updateClient);
-router.delete('/deleteClient/:id',            requireAuthUser, clientController.deleteClient);
-router.get('/getAllClients',                   requireAuthUser, clientController.getAllClients);
+router.post('/createClient', requireAuthUser, clientController.createClient);
+router.put('/updateClient/:id',requireAuthUser, clientController.updateClient);
+router.delete('/deleteClient/:id',requireAuthUser, clientController.deleteClient);
+router.get('/getAllClients',requireAuthUser, clientController.getAllClients);
 router.get('/getClientByMatriculeFiscale/:mf',requireAuthUser, clientController.getClientByMatriculeFiscale);
-router.get('/searchClientsByName',            requireAuthUser, clientController.searchClientsByName);
+router.get('/searchClientsByName',requireAuthUser, clientController.searchClientsByName);
+router.get('/:id', requireAuthUser, clientController.getClientById);
 
 module.exports = router;
